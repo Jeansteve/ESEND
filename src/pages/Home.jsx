7459bcf5030c1a80966861e3af8b3de4efb44723
jeansteve <ekenjean@gmail.com>
2026-03-24 09:36:29 +0000

    feat: implement Duo Expertise section and Portfolio Bento grid for ESEND

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from '../components/Sections/Hero'
 import PestSelector from '../components/Sections/PestSelector'
+import DuoExpertise from '../components/Sections/DuoExpertise'
+import PortfolioBento from '../components/Sections/PortfolioBento'
 import About from '../components/Sections/About'
 import Reviews from '../components/Sections/Reviews'
 import FormWizard from '../components/FormWizard/FormWizard'
@@ -8,14 +10,16 @@ import KnowledgeHub from '../components/Sections/KnowledgeHub'
 
 function Home() {
   return (
-    <>
+    <div className="bg-slate-950 overflow-x-hidden">
       <Hero />
       <PestSelector />
+      <DuoExpertise />
+      <PortfolioBento />
       <About />
       <Reviews />
       <KnowledgeHub />
       <FormWizard />
-    </>
+    </div>
   )
 }
 
