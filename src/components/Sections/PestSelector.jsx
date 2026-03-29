41efaccd491fbd16ccbfe09eff691ea61e9aad16
EKEN <steve@macbook-pro-de-eken.home>
2026-03-29 23:45:26 +0200

    style: make images float only on hover

--- src/components/Sections/PestSelector.jsx
+++ src/components/Sections/PestSelector.jsx
@@ -95,8 +95,13 @@ const PestSelector = () => {
                      {pest.icon}
                    </div>
                    <motion.img 
-                      animate={{ y: [0, -12, 0] }}
-                      transition={{ repeat: Infinity, duration: 4, ease: 'easeInOut' }}
+                      variants={{
+                        initial: { y: 0 },
+                        hover: { 
+                          y: [0, -12, 0],
+                          transition: { repeat: Infinity, duration: 3, ease: 'easeInOut' }
+                        }
+                      }}
                       src={pest.image} 
                       className="w-full h-full object-contain filter drop-shadow-[0_20px_30px_rgba(0,0,0,0.8)] z-10 lg:grayscale lg:brightness-75 lg:opacity-90 group-hover:grayscale-0 group-hover:brightness-100 group-hover:opacity-100 transition-all duration-700"
                       alt={pest.name}
