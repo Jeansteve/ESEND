892e7c2afde08ac68841672ae7c812fd45c294a9
EKEN <steve@macbook-pro-de-eken.home>
2026-04-01 17:24:44 +0200

    feat: show Header CTA immediately on non-home pages

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -75,7 +75,7 @@ const Header = () => {
           ))}
           
           <AnimatePresence>
-            {showCTA && (
+            {(location.pathname !== '/' || showCTA) && (
               <motion.div
                 initial={{ opacity: 0, x: 20 }}
                 animate={{ opacity: 1, x: 0 }}
